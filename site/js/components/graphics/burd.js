dc1c1413a5f2613a3d8b32563d9e6062a407de1e
B Paul Anderson <benpaul.anderson@gmail.com>
2015-12-13 21:36:07 -0600

    Multiple shapes/non-circles

--- site/js/components/graphics/burd.js
+++ site/js/components/graphics/burd.js
@@ -5,10 +5,28 @@ var BurdGraphicsComponent = function (entity) {
 };
 
 BurdGraphicsComponent.prototype.draw = function (context) {
-  context.fillStyle = '#aba4f4';
+
+  // Circle
   context.beginPath();
+  context.fillStyle = '#aba4f4';
   context.arc(0, 200, 70, 0, 2 * Math.PI);
   context.fill();
+
+  // Rectangle
+  context.beginPath();
+  context.fillStyle = '#299';
+  context.fillRect(100, 80, 150, 90);
+
+  // Triangle
+  context.beginPath();
+  context.strokeStyle = '#f00';
+  context.lineWidth = 10;
+  context.moveTo(200, 150);
+  context.lineTo(400, 150);
+  context.rotate(Math.PI / 3);
+  context.lineTo(400, 150);
+  context.closePath();
+  context.stroke();
 };
 
 module.exports = BurdGraphicsComponent;
