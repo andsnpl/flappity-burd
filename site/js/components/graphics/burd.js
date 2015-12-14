05b7d65f5328d5666218dcb06abe9b35e0f19eb2
B Paul Anderson <benpaul.anderson@gmail.com>
2015-12-13 21:25:44 -0600

    Change color

--- site/js/components/graphics/burd.js
+++ site/js/components/graphics/burd.js
@@ -5,6 +5,7 @@ var BurdGraphicsComponent = function (entity) {
 };
 
 BurdGraphicsComponent.prototype.draw = function (context) {
+  context.fillStyle = '#aba4f4';
   context.beginPath();
   context.arc(50, 50, 10, 0, 2 * Math.PI);
   context.fill();
