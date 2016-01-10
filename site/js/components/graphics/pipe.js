5a3e06ac7bad9f4e99b9ee7b9ed862d8112742f9
B Paul Anderson <benpaul.anderson@gmail.com>
2016-01-10 14:04:13 -0600

    realistic pipes

--- site/js/components/graphics/pipe.js
+++ site/js/components/graphics/pipe.js
@@ -8,6 +8,8 @@
  */
 var PipeGraphicsComponent = function PipeGraphicsComponent(entity) {
   this.entity = entity;
+  this.image = new Image();
+  this.image.src = 'images/pipe-top.png';
 };
 
 PipeGraphicsComponent.prototype.draw = function (context) {
@@ -15,12 +17,15 @@ PipeGraphicsComponent.prototype.draw = function (context) {
       width = this.entity.width,
       height = this.entity.height,
       attachT = this.entity.attach === 'T'; // Is this a top pipe or bottom?
-                  // (determines the color to paint)
 
   context.save();
   context.translate(position.x - (width / 2), attachT ? 1 - height : 0);
-  context.fillStyle = attachT ? 'red' : 'blue';
+  context.fillStyle = '#07c300';
   context.fillRect(0, 0, width, height);
+
+  // Draw the lip of the pipe
+  context.translate(-0.005, attachT ? 0 : height - 0.05);
+  context.drawImage(this.image, 0, 0, width + 0.01, 0.05);
   context.restore();
 };
 
